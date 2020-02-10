7f6919caed9016b418d108fdb7f040727c7efa91
ocurtis <olly_915@hotmail.com>
2020-02-10 21:07:26 +0000

    Updated min SDK to 21 to target 85% of users
    Updated min SDK to 21 in the main app to target 85% of users. Users will still need to be min API 24 to use AR mode.

--- NativeAndroidApp/app/src/main/java/com/lush_digital_/unity_android_shopping_app/ui/knot_wrap_experience/sceneform_ar/ARActivity.kt
+++ NativeAndroidApp/app/src/main/java/com/lush_digital_/unity_android_shopping_app/ui/knot_wrap_experience/sceneform_ar/ARActivity.kt
@@ -3,8 +3,10 @@ package com.lush_digital_.unity_android_shopping_app.ui.knot_wrap_experience.sce
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import com.lush_digital_.unity_android_shopping_app.R
 import com.unity3d.player.UnityPlayerActivity
+import kotlinx.android.synthetic.main.activity_ar.*
 
 class ARActivity : AppCompatActivity() {
 
@@ -14,10 +16,24 @@ class ARActivity : AppCompatActivity() {
         btnLoadUnity()
     }
 
-
     fun btnLoadUnity(){
-        val intent = Intent(this, UnityPlayerActivity::class.java)
-        intent.putExtra("arguments", "KnotWrapAR")
-        startActivity(intent)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            // only for Nougat and newer versions
+
+            val intent = Intent(this, UnityPlayerActivity::class.java)
+            intent.putExtra("arguments", "KnotWrapAR")
+            startActivity(intent)
+
+        }else{
+
+           snackbar("This Feature Requires OS version Nougat and above")
+        }
+    }
+
+    fun snackbar(
+    message: String
+    ) {
+        Snackbar.make(textView1, message, Snackbar.LENGTH_LONG).show()
     }
 }
