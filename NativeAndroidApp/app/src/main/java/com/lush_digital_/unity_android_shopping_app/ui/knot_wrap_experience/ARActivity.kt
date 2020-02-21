9442599345a879a7e470cb171bb612f40245aee3
ocurtis <olly_915@hotmail.com>
2020-02-21 23:22:23 +0000

    Added seeker bar to animation 1

--- NativeAndroidApp/app/src/main/java/com/lush_digital_/unity_android_shopping_app/ui/knot_wrap_experience/ARActivity.kt
+++ NativeAndroidApp/app/src/main/java/com/lush_digital_/unity_android_shopping_app/ui/knot_wrap_experience/ARActivity.kt
@@ -1,19 +1,14 @@
 package com.lush_digital_.unity_android_shopping_app.ui.knot_wrap_experience
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Point
 import android.graphics.Rect
-import android.hardware.Sensor
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import com.company.product.OverrideUnityActivity
 import com.lush_digital.knotwrappoc.ui.presentation.pagination.PaginationFragment
 import com.lush_digital_.unity_android_shopping_app.R
@@ -33,8 +28,6 @@ class ARActivity : OverrideUnityActivity() {
     private var popupMenu = Menu()
     private var playButton: Button? = null
     private var pauseButton: Button? = null
-    private var isPlayButtonVisible: Boolean? = false
-    private var isPauseButtonVisible: Boolean? = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -114,7 +107,6 @@ class ARActivity : OverrideUnityActivity() {
             UnityPlayer.UnitySendMessage("HelloAR Controller", "pauseAnimation", "")
 
         }
-
     }
 
     private fun playAnimation() {
